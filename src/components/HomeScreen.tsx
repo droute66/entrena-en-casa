4a1c05d733583bff023bd5a97e08c90b71d16246
Daniel <droute66@gmail.com>
2026-09-09 15:12:11 -0300

    Fix responsive progreso

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -78,7 +78,7 @@ export function HomeScreen({ progress, onStart }: HomeScreenProps) {
           {/* PROGRESO */}
 
           <div className="lg:col-span-2">
-            <div className="relative overflow-hidden bg-pine-deep border-2 border-lime/30 rounded-xl shadow-chalk p-6 sm:p-7 h-full">
+            <div className="relative w-full max-w-full min-w-0 overflow-hidden bg-pine-deep border-2 border-lime/30 rounded-xl shadow-chalk p-4 sm:p-7 h-full">
 
               <div className="absolute inset-0 bg-diag-dark" aria-hidden />
 
